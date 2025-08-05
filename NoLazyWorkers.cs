19ce87cd14dbac382c8a1ef17346f86bf2184fa0
archenovalis <archelaus.novalis@gmail.com>
2025-08-04 20:00:52 -0600

    griditemloader loadandcreate fix

--- NoLazyWorkers.cs
+++ NoLazyWorkers.cs
@@ -743,11 +743,13 @@ namespace NoLazyWorkers
     }
   }
 
-  [HarmonyPatch(typeof(GridItemLoader), "LoadAndCreate")]
+  [HarmonyPatch(typeof(GridItemLoader))]
   public class GridItemLoaderPatch
   {
     public static Dictionary<string, GridItem> LoadedGridItems = [];
 
+    [HarmonyPostfix]
+    [HarmonyPatch("LoadAndCreate")]
     static void Postfix(string mainPath, GridItem __result)
     {
       try
